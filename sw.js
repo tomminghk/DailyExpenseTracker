e4d8965ce966b6934d08b852b61ce6fa424d8b48
tomminghk <48377850+tomminghk@users.noreply.github.com>
2026-04-17 15:10:54 +0800

    Create sw.js
    New file suggested by Gemini 3.1 Pro

---
+++ sw.js
@@ -0,0 +1,5 @@
+// A minimal service worker to satisfy Chrome's PWA install requirement
+self.addEventListener('fetch', function(event) {
+    // We don't need to do anything here right now, 
+    // just having this listener makes Chrome happy!
+});
